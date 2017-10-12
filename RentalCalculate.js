29bfafa8cb2898677cd0d77cbe377625a10c90a6
santosh <santu.kondapally@gmail.com>
2017-10-12 06:47:57 -0500

    das
    dasdasda

--- RentalCalculate.js
+++ RentalCalculate.js
@@ -1,15 +1,6 @@
 function rentalCarCost(noDays) {
-    // Your solution here
-    var result = 0;
-    var carcost = noDays * 40;
-    if (noDays >= 3 && noDays < 7) {
-
-        result = carcost - 20;
-
-    } else if (noDays >= 7) {
-        result = carcost - 50;
-    } else {
-        result = carcost;
-    }
-    return result;
+  var totalCost = noDays * 40;
+  if (noDays >= 7){totalCost -= 50;}
+  else if (noDays >= 3){ totalCost -= 20;}
+  return totalCost;
 }
